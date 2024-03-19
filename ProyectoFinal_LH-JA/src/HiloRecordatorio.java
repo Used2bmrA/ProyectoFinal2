dd89bd991da8513a9a50255c5526496283bb72ab
NadShatts <josedaniel202006@gmail.com>
2024-03-19 15:14:20 -0600

    Avance del hilo

--- ProyectoFinal_LH-JA/src/HiloRecordatorio.java
+++ ProyectoFinal_LH-JA/src/HiloRecordatorio.java
@@ -1,5 +1,6 @@
 
 import java.util.Calendar;
+import java.util.Date;
 import javax.swing.JOptionPane;
 import javax.swing.JSpinner;
 
@@ -14,9 +15,11 @@ import javax.swing.JSpinner;
  */
 public class HiloRecordatorio extends Thread{
     private Actividad actividades;
+    private int minutosActividad;
 
-    public HiloRecordatorio(Actividad actividades) {
+    public HiloRecordatorio(Actividad actividades, int minutosActividad) {
         this.actividades = actividades;
+        this.minutosActividad = minutosActividad;
     }
 
     public Actividad getActividades() {
@@ -26,9 +29,38 @@ public class HiloRecordatorio extends Thread{
     public void setActividades(Actividad actividades) {
         this.actividades = actividades;
     }
+
+    public int getMinutosActividad() {
+        return minutosActividad;
+    }
+
+    public void setMinutosActividad(int minutosActividad) {
+        this.minutosActividad = minutosActividad;
+    }
+
+    
     
     public void run(){
+       while(true){
+           try {
+                Date ahora = new Date();
+         
+                Date horaInicio = new Date(ahora.getTime() + minutosActividad * 60000);
+
+                long tiempoRestante = actividades.getRecordatorioMinutos().getTime() - horaInicio.getTime();
+
+                if (tiempoRestante <= 0) {
+                    JOptionPane.showMessageDialog(null, "Es hora de tu actividad llamada" + actividades.getTitulo() + "!");
+                    break;
+                }
+                Thread.sleep(tiempoRestante - 60000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+           
+       }
         
     }
 
-}
+
